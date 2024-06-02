4a67d05d20d539e8817885bcef811c00967db454
NguyenVanTaiAnh <97070517+NguyenVanTaiAnh@users.noreply.github.com>
2024-06-02 14:07:27 +0700

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,10 +4,10 @@ const { test, expect } = require('@playwright/test');
 test('test', async ({ page }) => {
   await page.goto('https://qldtbeta.phenikaa-uni.edu.vn/congsinhvien/login.aspx');
   await page.getByRole('link', { name: 'Sign in using Microsoft' }).click();
-  await page.getByLabel('account@phenikaa-uni.edu.vn').fill('21010666@st.phenikaa-uni.edu.vn');
+  await page.getByLabel('account@phenikaa-uni.edu.vn').fill('21010645@st.phenikaa-uni.edu.vn');
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('0983419372Hoan.');
+  await page.getByPlaceholder('Password').fill('Taianh19112003@');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByLabel('Don\'t show this again').check();
   await page.getByRole('button', { name: 'Yes' }).click();
